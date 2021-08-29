24bb44451c0dcfcd6a7faa4e2bb3fc309a9394f6
Steven Flintham <sgf@lemma.co.uk>
2021-08-29 22:38:34 +0100

    Snapshot

--- a.py
+++ a.py
@@ -1,9 +1,19 @@
 from __future__ import print_function
 
-memory = [0] * 64*1024
+def disassemble_instruction(addr):
+    assert what[addr] is None
+    return []
+
+memory = [None] * 64*1024
+labels = [None] * 64*1024
+what = [None] * 64*1024
 
 with open("/home/steven/src/anfs-disassembly/roms/anfs418.orig", "rb") as f:
     memory[0x8000:] = f.read()
 
-print(memory[0x8003])
-print(memory[0x8004])
+entry_points = [0x8003]
+
+while len(entry_points) > 0:
+    entry_point = entry_points.pop(0)
+    if what[entry_point] is None:
+        entry_points.extend(disassemble_instruction(entry_point))
