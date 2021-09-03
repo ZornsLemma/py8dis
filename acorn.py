caace9888d4780517057d825cbaffe4b489dd841
Steven Flintham <sgf@lemma.co.uk>
2021-09-03 20:18:38 +0100

    Add more values to acorn.label_os_entry_points()

--- acorn.py
+++ acorn.py
@@ -2,26 +2,66 @@ from commands import *
 import utils
 
 # TODO: Do this automatically just from "import acorn"?
+# TODO: Rename now it's not pure entry points
+# TODO: Split this up somehow into "tube or host" and "just host"?
 def label_os_entry_points():
-    # TODO: Ideally we wouldn't show any labels in the output that aren't referenced
-    optional_label(0xffb9, "osrdrm")
-    optional_label(0xffbf, "osevent")
+    optional_label(0x00f2, "os_text_ptr")
+    optional_label(0x00f4, "romsel_copy")
+    optional_label(0x00f6, "osrdsc_ptr")
+
+    # TODO: These aren't ideal, as e.g. "STA &201" will not be interpreted as userv+1.
+    optional_label(0x0200, "userv")
+    optional_label(0x0202, "brkv")
+    optional_label(0x0204, "irq1v")
+    optional_label(0x0206, "irq2v")
+    optional_label(0x0208, "cliv")
+    optional_label(0x020a, "bytev")
+    optional_label(0x020c, "wordv")
+    optional_label(0x020e, "wrchv")
+    optional_label(0x0210, "rdchv")
+    optional_label(0x0212, "filev")
+    optional_label(0x0214, "argsv")
+    optional_label(0x0216, "bgetv")
+    optional_label(0x0218, "bputv")
+    optional_label(0x021a, "gbpbv")
+    optional_label(0x021c, "findv")
+    optional_label(0x021e, "fscv")
+    optional_label(0x0220, "evntv")
+    optional_label(0x0222, "uptv")
+    optional_label(0x0224, "netv")
+    optional_label(0x0226, "vduv")
+    optional_label(0x0228, "keyv")
+    optional_label(0x022a, "insv")
+    optional_label(0x022c, "remv")
+    optional_label(0x022e, "cnpv")
+    optional_label(0x0230, "ind1v")
+    optional_label(0x0232, "ind2v")
+    optional_label(0x0234, "ind3v")
+
+    optional_label(0xfe30, "romsel")
+
+    optional_label(0xffb9, "osrdsc")
+    optional_label(0xffbc, "vduchr")
+    optional_label(0xffbf, "oseven")
     optional_label(0xffc2, "gsinit")
     optional_label(0xffc5, "gsread")
     optional_label(0xffc8, "nvrdch")
     optional_label(0xffcb, "nvwrch")
     optional_label(0xffce, "osfind")
+    optional_label(0xffd1, "osgbpb")
     optional_label(0xffd4, "osbput")
     optional_label(0xffd7, "osbget")
     optional_label(0xffda, "osargs")
     optional_label(0xffdd, "osfile")
-    optional_label(0xfff1, "osword")
-    optional_label(0xfff4, "osbyte")
     optional_label(0xffe0, "osrdch")
     optional_label(0xffe3, "osasci")
     optional_label(0xffe7, "osnewl")
+    optional_label(0xffee, "oswrcr")
     optional_label(0xffee, "oswrch")
+    optional_label(0xfff1, "osword")
+    optional_label(0xfff4, "osbyte")
     optional_label(0xfff7, "oscli")
+
 
 def is_sideways_rom():
     comment(0x8000, "Sideways ROM header")
