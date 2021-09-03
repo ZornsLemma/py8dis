79b25dcd10113975d351501028405435eac96f06
Steven Flintham <sgf@lemma.co.uk>
2021-09-03 15:33:41 +0100

    Add TODO

--- memory.py
+++ memory.py
@@ -10,5 +10,8 @@ lower_case = [True]
 
 inline_comment_column = [60]
 
+# TODO: Perhaps provide foo() [get] and set_foo(x) functions for all the
+# config, then I can avoid things like formatter[0].blah() and write formatter().blah() instead,
+# which saves a character and is probably less confusing.
 def set_lower_case(b):
     lower_case[0] = b
