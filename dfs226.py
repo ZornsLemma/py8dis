cb4d9ef93dd356acb3677d5f833ef330711db47a
Steven Flintham <sgf@lemma.co.uk>
2021-09-04 16:27:10 +0100

    WIP DFS 2.26 disassembly

--- dfs226.py
+++ dfs226.py
@@ -7,4 +7,10 @@ set_output_filename("dfs226.rom")
 acorn.label_os_entry_points() # TODO: label_os_api()?
 acorn.is_sideways_rom() # TODO: rename?
 
+constant(0xc6, "osbyte_rw_exec_handle")
+expr(0x9e31, "osbyte_rw_exec_handle")
+
+label(0x83dc, "inc16_ae")
+label(0x9ae5, "osbyte_x0_yff")
+
 go()
