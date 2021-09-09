a8b355f18335fa4f97297c9e07ef728f46973afa
Steven Flintham <sgf@lemma.co.uk>
2021-09-09 20:44:14 +0100

    Tweak basic4.py

--- basic4.py
+++ basic4.py
@@ -8,6 +8,15 @@ set_output_filename("basic4.rom")
 acorn.label_os_entry_points() # TODO: label_os_api()?
 acorn.is_sideways_rom() # TODO: rename?
 
-wordentry(0x8826, 123) # XXX: 123 is a guess
+wordentry(0x8826, 121) # XXX: table size is a guess
+
+# XXX: l907d has a jsr which is followed by a BRA opcode, but it isn't
+# disassembled because the BRA operand is already being disassembled as
+# a tsb. This may well be a deliberate coding trick rather than a glitch
+# in the disassembly.
+
+# ENHANCE: Add a brk() hook to trace6502? This would allow us to disassemble
+# OS errors without letting autostring() do it - it's not a big deal, but it
+# can interpret error codes as ASCII.
 
 go() # TODO: rename if we keep trace() outside
