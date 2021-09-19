4a4f06e0356d994f2f143fbb3f5452188c8c35fd
Steven Flintham <sgf@lemma.co.uk>
2021-09-19 19:20:57 +0100

    WIP analysis

--- py8dis/trace65c02.py
+++ py8dis/trace65c02.py
@@ -34,37 +34,41 @@ class OpcodeJmpAbsX(OpcodeAbs):
         return [None]
 
 
+def update_z(addr, state):
+    state['z'] = None
+
+
 # ENHANCE: On a 65C02 undefined opcodes are guaranteed to be NOPs of varying
 # lengths. These are *not* implemented here, since most code probably won't be
 # executing them and it's probably better to reduce the chances of arbitrary
 # data being traced as code. All the same, it might be nice to allow the user to
 # opt in to disassembling these.
 opcodes.update({
-    0x04: OpcodeZp("TSB"),
-    0x0c: OpcodeDataAbs("TSB"),
-    0x12: OpcodeZp("ORA", ")"),
-    0x14: OpcodeZp("TRB"),
-    0x1a: OpcodeImplied("INC A"),
-    0x1c: OpcodeDataAbs("TRB"),
-    0x32: OpcodeZp("AND", ")"),
-    0x34: OpcodeZp("BIT", ",X"),
-    0x3c: OpcodeDataAbs("BIT", ",X"),
-    0x3a: OpcodeImplied("DEC A"),
-    0x52: OpcodeZp("EOR", ")"),
-    0x5a: OpcodeImplied("PHY"),
-    0x64: OpcodeZp("STZ"),
-    0x72: OpcodeZp("ADC", ")"),
-    0x74: OpcodeZp("STZ", ",X"),
-    0x7a: OpcodeImplied("PLY"),
+    0x04: OpcodeZp("TSB", update=update_z),
+    0x0c: OpcodeDataAbs("TSB", update=update_z),
+    0x12: OpcodeZp("ORA", ")", update=update_anz),
+    0x14: OpcodeZp("TRB", update=update_z),
+    0x1a: OpcodeImplied("INC A", update=make_increment('a')),
+    0x1c: OpcodeDataAbs("TRB", update=update_z),
+    0x32: OpcodeZp("AND", ")", update=update_anz),
+    0x34: OpcodeZp("BIT", ",X", update=update_bit),
+    0x3c: OpcodeDataAbs("BIT", ",X", update=update_bit),
+    0x3a: OpcodeImplied("DEC A", update=make_decrement('a')),
+    0x52: OpcodeZp("EOR", ")", update=update_anz),
+    0x5a: OpcodeImplied("PHY", update=neutral),
+    0x64: OpcodeZp("STZ", update=neutral),
+    0x72: OpcodeZp("ADC", ")", update=update_adc_sbc),
+    0x74: OpcodeZp("STZ", ",X", update=neutral),
+    0x7a: OpcodeImplied("PLY", update=update_ynz),
     0x7c: OpcodeJmpAbsX(),
     0x80: OpcodeUnconditionalBranch("BRA"),
-    0x89: OpcodeImmediate("BIT"),
-    0x92: OpcodeZp("STA", ")"),
-    0x9c: OpcodeDataAbs("STZ"),
-    0x9e: OpcodeDataAbs("STZ", ",X"),
-    0xb2: OpcodeZp("LDA", ")"),
-    0xd2: OpcodeZp("CMP", ")"),
-    0xda: OpcodeImplied("PHX"),
-    0xf2: OpcodeZp("SBC", ")"),
-    0xfa: OpcodeImplied("PLX"),
+    0x89: OpcodeImmediate("BIT", update=update_z), # BIT immediate only affects Z
+    0x92: OpcodeZp("STA", ")", update=neutral),
+    0x9c: OpcodeDataAbs("STZ", update=neutral),
+    0x9e: OpcodeDataAbs("STZ", ",X", update=neutral),
+    0xb2: OpcodeZp("LDA", ")", update=update_anz),
+    0xd2: OpcodeZp("CMP", ")", update=update_nzc),
+    0xda: OpcodeImplied("PHX", update=neutral),
+    0xf2: OpcodeZp("SBC", ")", update=update_adc_sbc),
+    0xfa: OpcodeImplied("PLX", update=update_xnz),
 })
