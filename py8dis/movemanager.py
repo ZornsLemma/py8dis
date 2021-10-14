4ffd034aaed0542a276b24afe295bcd08d4b1d11
Steven Flintham <sgf@lemma.co.uk>
2021-10-14 17:21:13 +0100

    WIP

--- py8dis/movemanager.py
+++ py8dis/movemanager.py
@@ -19,12 +19,15 @@ import contextlib
 
 import utils
 
-active_move_ids = [0]
+# TODO: Rename?
+base_move_id = 0
+
+active_move_ids = []
 
 # TODO: Note that move_definitions has to be interpreted "as a whole", since later moves can "steal" binary addresses from earlier moves. I don't think this is a problem - and the whole point is to allow the user to do things like move a big chunk of code which gets relocated at runtime as a whole and then override that for a fairly small chunks witin that big chunk that the copied elsewhere.
 move_definitions = [(0, 0, 0x10000)]
 
-move_id_for_binary_addr = [0] * 0x10000
+move_id_for_binary_addr = [base_move_id] * 0x10000
 
 def add_move(dest, source, length):
     assert utils.is_valid_addr(dest)
@@ -40,7 +43,7 @@ def add_move(dest, source, length):
     return move_id
 
 def is_valid_move_id(move_id):
-    return 0 <= move_id < len(move_definitions)
+    return move_id == base_move_id or 0 <= move_id < len(move_definitions)
 
 # TODO: Name for this function is perhaps not ideal
 @contextlib.contextmanager
@@ -79,43 +82,52 @@ def r2b(runtime_addr):
     # my way through this.
     move_ids_for_runtime_addr = collections.defaultdict(list) # TODO: set not list??
     for binary_addr, move_id in enumerate(move_id_for_binary_addr):
-        move_ids_for_runtime_addr[b2r(binary_addr)].append(move_id)
+        if move_id != base_move_id: # TODO: special case feels a bit awkward
+            move_ids_for_runtime_addr[b2r(binary_addr)].append(move_id)
     relevant_move_ids = move_ids_for_runtime_addr[runtime_addr]
-    assert len(relevant_move_ids) > 0
-    for move_id in active_move_ids[::-1]:
-        if move_id in relevant_move_ids:
-            move_dest, move_source, move_length = move_definitions[move_id]
-            assert move_dest <= runtime_addr < (move_dest + move_length)
-            return (move_source + (runtime_addr - move_dest), move_id)
-    assert False # TODO: currently not clear to me if this is possible...
-
+    print("XXX", relevant_move_ids)
+    if len(relevant_move_ids) == 0:
+        return runtime_addr, base_move_id
+    selected_move_id = None
+    if len(relevant_move_ids) == 1:
+        selected_move_id = relevant_move_ids[0]
+    else:
+        for move_id in active_move_ids[::-1]:
+            if move_id in relevant_move_ids:
+                selected_move_id = move_id
+                break
+    if selected_move_id is None:
+        return (None, None)
+    move_dest, move_source, move_length = move_definitions[selected_move_id]
+    assert move_dest <= runtime_addr < (move_dest + move_length)
+    return (move_source + (runtime_addr - move_dest), selected_move_id)
 
 if __name__ == "__main__":
     id1 = add_move(0x70, 0x1900, 10)
     id2 = add_move(0x70, 0x2000, 8)
 
-    assert move_id_for_binary_addr[0x70] == 0
+    assert move_id_for_binary_addr[0x70] == base_move_id
     assert move_id_for_binary_addr[0x1900] == id1
     assert move_id_for_binary_addr[0x2000] == id2
-    assert move_id_for_binary_addr[0x2000 + 8] == 0
+    assert move_id_for_binary_addr[0x2000 + 8] == base_move_id
 
     assert b2r(0x70) == 0x70
     assert b2r(0x1900) == 0x70
     assert b2r(0x2000) == 0x70
     assert b2r(0x2000 + 8) == 0x2000 + 8
 
-    print("QQQ", hex(r2b(0x70))) # TODO: what *should* happen here? it is *probably* correct - ie we should assert this - that this returns 0x70, since every runtime address has the corresponding binary address moved onto it by move ID 0 if nothing else is actively overriding that - but I would like to think about it a bit more before "committing" to this
+    print("QQQ", r2b(0x70)) # TODO: what *should* happen here? it is *probably* correct - ie we should assert this - that this returns 0x70, since every runtime address has the corresponding binary address moved onto it by move ID 0 if nothing else is actively overriding that - but I would like to think about it a bit more before "committing" to this
 
-    assert active_move_ids == [0]
+    assert active_move_ids == []
     with moved(id2):
-        assert active_move_ids == [0, id2]
-        assert r2b(0x70) == 0x2000
-        assert r2b(0x2008) == 0x2008
+        assert active_move_ids == [id2]
+        assert r2b(0x70) == (0x2000, id2)
+        assert r2b(0x2008) == (0x2008, base_move_id)
         with moved(id1):
-            assert active_move_ids == [0, id2, id1]
-            assert r2b(0x70) == 0x1900
-            assert r2b(0x2008) == 0x2008
-        assert active_move_ids == [0, id2]
-        assert r2b(0x70) == 0x2000
-        assert r2b(0x2008) == 0x2008
-    assert active_move_ids == [0]
+            assert active_move_ids == [id2, id1]
+            assert r2b(0x70) == (0x1900, id1)
+            assert r2b(0x2008) == (0x2008, base_move_id)
+        assert active_move_ids == [id2]
+        assert r2b(0x70) == (0x2000, id2)
+        assert r2b(0x2008) == (0x2008, base_move_id)
+    assert active_move_ids == []
