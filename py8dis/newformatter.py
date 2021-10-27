35a986a2e313e26e48d15f319c650cbf508e9522
Steven Flintham <sgf@lemma.co.uk>
2021-10-27 22:29:02 +0100

    WIP formatter changes

--- py8dis/newformatter.py
+++ py8dis/newformatter.py
@@ -19,6 +19,7 @@ def make_indent(n):
 def add_hex_dump(binary_addr, length, s):
     # TODO: config should allow this to be a no-op
     # TODO: capped_length support as in utils.add_hex_dump()? May want to be a bit cleverer here, not sure yet
+    # TODO: support for showing ASCII alongside hex dump - in order for this to align nicely we need something like capped_length so we can ensure the lhs of the ASCII dump is consistent
     s = tab_to(s, hex_dump_column)
     s += config.formatter().comment_prefix() + " "
     s += formatter().hex4(binary_addr) + " "
@@ -41,6 +42,42 @@ def format_classification_line(binary_addr, length, core_str):
     # TODO: multiline things
     s = make_indent(indent) + core_str
     return add_hex_dump(binary_addr, length, s)
+
+# TODO: Highly experimental sketch for byte/word emitter
+# TODO: We might want Byte/Word objects to have a format function (a bit like format_hint for individual bytes) and allow the user to control it (poss via helper fns) - that way they could call this function with a None argument to get auto-column-calculation or an integer argument to specify "use n columns" or a variant of this function to get "no alignment but basic data-item-oriented-word-wrapping" output.
+def format_data_block(TODO):
+    indent = 1 # TODO: variable indent
+    if TODO:
+        # TODO: Old code used to force_hex2=True on get_constant8, I may want to do that or something *similar* here - I think the idea was to ensure nice column alignment without the different lengths of things like "3", "&f" and "&2f" breaking things. I may just be smarter about alignment and not need to do any kind of forcing. If I do need to force, I might want to force leading spaces to a fixed width (probably 3 chars) *if* using a simple "numeric" formatter.
+        data = list(get_constant8(binary_addr + i) for i in range(self._length))
+        data_prefix = config.formatter.byte_prefix()
+    else:
+        data = list(get_constant16(binary_addr + i) for i in range(0, self._length, 2))
+        data_prefix = config.formatter.word_prefix()
+    prefix = make_indent(indent) + word_prefix + " "
+    # TODO: We should allow the user to specify a number of data item columns - that would be part of
+    # the Byte/Word object and we would just use it here instead of deciding for ourselves
+    # TODO: We should also support "just emit with no padding or attempt to align columns but not spilling past data_width unless a single item forces it", a pseudo "word wrapping" style
+    # TODO: We might want to use a different value instead of hex_dump_column, e.g.
+    # absolute_max_width (80/100/whatever) - "hex dump max width or 0 if no hex dump".
+    separator = ", "
+    # TODO: We subtract 1 to leave a space before the hex dump, but that's not ideal if there
+    # is no hex dump; we could have used an extra column.
+    data_width = hex_dump_column - 1 - len(prefix)
+    #shortest_item = min(3, min(len(x) for x in data))
+    longest_item = max(len(x) for x in data)
+    # We add len(separator) to data_width because if we there are n items on a
+    # line we only need n-1 separators, but the divisor assumes every item
+    # includes a separator. TODO: Probably correct but think about it...
+    data_columns = min(1, (data_width + len(separator)) // (longest_item + len(separator)))
+    result = []
+    for i in range(0, len(data), data_columns):
+        items_on_line = min(len(data) - i, data_columns)
+        core_str = data_prefix + separator.join("%-*s" % (longest_item, x) for x in data[i:i+data_columns])
+        # TODO: We're double-indenting here - prefix includes indent (desirable so we can calculate chars-per-line for our data) and format_classification_line() will add it as well
+        result.append(format_classification_line(binary_addr + i, items_on_line, core_str))
+    assert False
+    return result
 
 def int_formatter(n, bits):
     if n < 10:
