fb583e2c3f6624d5801131210e80ab140c9f1c9c
Steven Flintham <sgf@lemma.co.uk>
2021-12-31 01:38:14 +0000

    Fix/hack round xa pseudopc problem

--- py8dis/xa.py
+++ py8dis/xa.py
@@ -4,6 +4,7 @@ import sys
 import classification
 import config
 import disassembly # TODO?
+import movemanager
 import utils
 
 config.set_formatter(sys.modules[__name__])
@@ -54,11 +55,19 @@ def code_start(start_addr, end_addr):
 def code_end():
     return []
 
+# TODO: pseudopc_index is a bit of a hack. This problem is that xa (not unreasonably, probably) won't accept forward references in the "* = ..." line emitted by pseudopc_end(). We need to ensure there are labels defined at the start and end of the pseudopc block so they can be used in pseudopc_end without needing any forward references. This hack seems to work in practice, but note that *if* there's already a label in one of these places the labels we add here are redundant and may not be referenced. I don't know if we can (easily/at all) detect when this happens; perhaps we can/should add a "weak" label definition which is only emitted if it's used, though that might be more complex than necessary. I *think* the fact we are using the move ID with the labels here and in pseudopc_end() means we won't end up having get_label() decide to return us a label name for the right address which is defined elsewhere (and potentially after) in the file. If push came to shove we could just forcibly use the pseudopc_start_xxx labels in pseudopc_end() instead of going via get_label(); that might in fact be the cleanest solution all round, although it wouldn't be unreasonable for a user to want to be able to control the label names. Perhaps moves could be given an optional name which is used when generating these labels instead of auto-generating them using a numeric sequence, though that might be a bit assembler-specific.
+pseudopc_index=0
 def pseudopc_start(dest, source, length):
+    move_id = movemanager.move_id_for_binary_addr[source]
+    global pseudopc_index
+    disassembly.add_label(dest, "pseudopc_start_%d" % pseudopc_index, move_id)
+    disassembly.add_label(dest + length, "pseudopc_end_%d" % pseudopc_index, move_id)
+    pseudopc_index += 1
     return ["* = %s" % hex(dest)]
 
 def pseudopc_end(dest, source, length):
-    return [utils.LazyString("* = %s + (%s - %s)", disassembly.get_label(source, source), disassembly.get_label(dest + length, source), disassembly.get_label(dest, source))]
+    move_id = movemanager.move_id_for_binary_addr[source]
+    return [utils.LazyString("* = %s + (%s - %s)", disassembly.get_label(source, source, move_id), disassembly.get_label(dest + length, source, move_id), disassembly.get_label(dest, source, move_id))]
 
 def disassembly_end():
     result = []
