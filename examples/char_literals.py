5581cd8eb00522cb666c557a8dd50f82f215326d
Toby Nelson <tobymnelson@gmail.com>
2025-03-05 08:35:03 +0000

    Add example to test every character literal

---
+++ examples/char_literals.py
@@ -0,0 +1,15 @@
+from commands import *
+
+# Load the program to be disassembled into the debugger's memory.
+# The md5sum is optional but helps avoid confusion if there are
+# multiple versions of the same program.
+load(0x2000, "char_literals.orig", "6502")
+
+# Start tracing instructions at 0x2000.
+entry(0x2000)
+
+for i in range(0,256):
+    char(0x2001 + 2*i)
+
+# Use all the information provided to actually disassemble the program.
+go()
