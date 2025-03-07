b6d94f839f2331af7a4c7fd7b29887905cb5ca51
Toby Nelson <tobymnelson@gmail.com>
2025-03-07 09:02:16 +0000

    For 8080 code, labels at ret instructions are numerically ordered. (This is probably less commonly needed for 8080 code since it has conditional return instructions, but hey maybe it's useful.)

--- examples/python/example8080.py
+++ examples/python/example8080.py
@@ -1,7 +1,7 @@
 from commands import *
 
 # Example code from https://en.wikipedia.org/wiki/Intel_8080#Example_code
-load(0x1000, "orig/example8080.orig", "8080", "d3db354aac5d0f0d4d2527030d5aff1e")
+load(0x1000, "orig/example8080.orig", "8080")
 entry(0x1000)
 
 go()
